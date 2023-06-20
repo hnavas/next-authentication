434e0e6c293fdf50e3f1bb65d590d5462eac84f2
hnavas <hendri.navas@gmail.com>
2023-06-19 22:12:52 -0300

    Connect mongoDB and create Model User

---
+++ src/app/api/auth/signup/route.ts
@@ -0,0 +1,61 @@
+import {  NextResponse } from "next/server";
+import { connectDB } from "@/libs/mongodb";
+import User from "@/models/user";
+import bcrypt from "bcryptjs";
+
+export async function POST(request: Request) {
+  const { fullname, email, password } = await request.json();
+  console.log(fullname, email, password);
+
+  //Validar longitud del password
+  if (!password || password.length < 6) {
+    return NextResponse.json(
+      {
+        message: "Passaword must be at least 6 characters",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+  //Buscar si el email existe
+  try {
+    await connectDB();
+    const userFound = await User.findOne({email});
+
+    if (userFound) {
+      return NextResponse.json(
+        {
+          message: "Email already exists",
+        },
+        {
+          status: 409,
+        }
+      )
+    }
+    //Si no existe se encripta el password y se crea el User
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = new User({
+      email,
+      fullname,
+      password: hashedPassword,
+    });
+    const savedUser = await user.save();
+    console.log(savedUser);
+  
+    return NextResponse.json(savedUser);
+  } catch (error) {
+    console.log(error);
+    if (error instanceof Error) {
+      return NextResponse.json(
+        {
+          message: error.message
+        },
+        {
+          status: 400,
+        }
+      )
+    }
+  }
+  
+}
